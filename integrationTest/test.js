71e0129e7fc9ca8190ffdc790999defa412e1fae
Runda Li <s5099129@student.ad.griffith.edu.au>
2019-09-19 17:56:09 +1000

    finished integration test

--- integrationTest/test.js
+++ integrationTest/test.js
@@ -15,21 +15,29 @@ describe('Server test', function(){
     before(function() {console.log("before test");})
     after(function() {console.log("after test")})
 
-    describe('/add', ()=>{
-        it('should return all products', function(){
-            http.post('http://localhost:3000/add', {id: 1, name: 'product 1', description: 'iphone', price: 600, units: 1}, function(response){
-                assert.equal(response.statusCode, 200);
-
-                response.body.should.be.a('boolean');
-            })
-        })
-    })
-    // describe('/list', ()=>{
+    // describe('/add', ()=>{
     //     it('should return all products', function(){
-    //         http.get('http://localhost:3000/list', function(response){
+    //         http.post('http://localhost:3000/add', {id: 1, name: 'product 1', description: 'iphone', price: 600, units: 1}, function(response){
     //             assert.equal(response.statusCode, 200);
 
-    //             response.body.should.be.a('array');
+    //             response.body.should.be.a('boolean');
+    //         })
+    //     })
+    // })
+    // describe('/list', ()=>{
+    //     it('should return all products', function(){
+    //         http.get('http://localhost:3000/list', (res)=>{
+    //             assert.equal(res.statusCode, 200);
+
+    //             // res.body.should.be.a('array');
+    //             // console.log("type:", typeof(res.body))
+    //             // console.log("type:", typeof(res))
+    //             console.log("res: ", res.body)
+    //             var body = '';
+    //             res.on('data', function(d) {body += d;});
+    //             res.on('end', function(){
+    //                 assert.equal(body, [{"_id":"5d831be20b31e03c3cefa8ae","id":1,"name":"1","description":"1","price":1,"units":1}])
+    //             })
     //         })
     //     })
     // })
@@ -56,26 +64,105 @@ describe('Server test', function(){
     //             })
     //     })
     // })
-    // describe('/list', ()=>{
-    //     it('it should get all the products', (done)=>{
-    //         chai.request("http://localhost:3000").get('/list')
-    //             .end((err, res)=>{
-    //                 res.should.have.status(200);
-    //                 res.body.should.be.a('array');
-    //                 done();
-    //             })
-    //     })
-    // })
-    // describe('/list', ()=>{
-    //     it('it should get all the products', (done)=>{
-    //         chai.request("http://localhost:3000").get('/list')
-    //             .end((err, res)=>{
-    //                 res.should.have.status(200);
-    //                 res.body.length.should.be.eql(1);
-    //                 console.log(res.body[0].name)
-    //                 console.log(res.body.length)
-    //                 done();
-    //             })
-    //     })
-    // })
+    describe('/list', ()=>{
+        it('it should get all the products', ()=>{
+            chai.request("http://localhost:3000").get('/list')
+                .end((err, res)=>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+
+                })
+        })
+        it('it should get all the products', ()=>{
+            chai.request("http://localhost:3000").get('/list')
+                .end((err, res)=>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+
+                })
+        })
+    })
+
+    describe('/add', ()=>{
+        it('it should get all the products', ()=>{
+            chai.request("http://localhost:3000").post('/add')
+            .send({id: 2, name: "iphone", description: "iphone", price:600, units:1})
+                .end((err, res)=>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('boolean');
+     
+                })
+        })
+
+        it('it should get all the products', ()=>{
+            chai.request("http://localhost:3000").post('/add')
+            .send({id: 3, name: "iphone11", description: "iphone11", price:1000, units:1})
+                .end((err, res)=>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('boolean');
+     
+                })
+        })
+    })
+
+    describe('/deleteProduct', ()=>{
+        it('it should get all the products', ()=>{
+            chai.request("http://localhost:3000").post('/deleteProduct')
+            .send({productid: "5d83316e8f6baf46205839d7"})
+                .end((err, res)=>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+             
+                })
+        })
+        it('it should get all the products', ()=>{
+            chai.request("http://localhost:3000").post('/deleteProduct')
+            .send({productid: "5d8332802eef18041cac6c27"})
+                .end((err, res)=>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+
+                })
+        })
+    })
+
+    describe('/getProduct', ()=>{
+        it('it should get all the products', ()=>{
+            chai.request("http://localhost:3000").post('/getProduct')
+            .send({productid: "5d833040d08875277c1ce7a4"})
+                .end((err, res)=>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    console.log(res.body)
+                })
+        })
+        it('it should get all the products', ()=>{
+            chai.request("http://localhost:3000").post('/getProduct')
+            .send({productid: "5d833040d08875277c1ce7a4"})
+                .end((err, res)=>{
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    console.log(res.body)
+                })
+        })
+     
+    })
+    describe('/update', ()=>{
+        it('it should get all the products', ()=>{
+            chai.request("http://localhost:3000").post('/update')
+            .send({objid: "5d833040d08875277c1ce7a4", id: 3, name: "iphone12", description: "iphone12", price:1100, units:1})
+                .end((err, res)=>{
+                    res.should.have.status(200);
+ 
+                })
+        })
+        it('it should get all the products', ()=>{
+            chai.request("http://localhost:3000").post('/update')
+            .send({objid: "5d833040d08875277c1ce7a4", id: 2, name: "iphone111", description: "iphone111", price:100, units:1})
+                .end((err, res)=>{
+                    res.should.have.status(200);
+ 
+                })
+        })
+    })
 })
--- server/routes/remove.js
+++ server/routes/remove.js
@@ -7,14 +7,16 @@ module.exports = function(db,app,ObjectID){
         if (!req.body) {
          return res.sendStatus(400);
         }
-        
+        console.log(req.body);
+        console.log("ioqsjdoijqwoid");
+
         productID = req.body.productid;
         console.log(productID)
         //create a new mongo Object ID from the passed in _id
         var objectid = new ObjectID(productID);
         console.log(objectid)
         const collection = db.collection('products');
-        //Delete a single item based on its unique ID.
+        // Delete a single item based on its unique ID.
         collection.deleteOne({_id:objectid},(err,docs)=>{
             //get a new listing of all items in the database and return to client.
             collection.find({}).toArray((err,data)=>{
